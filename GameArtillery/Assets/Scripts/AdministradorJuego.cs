dfa382ea344c9de9af3bee9f737f965fd1a9b155
Jully Kado <jully_000@hotmail.com>
2022-03-11 13:25:50 -0600

    Movimiento
    Creación del movimiento del canon

---
+++ GameArtillery/Assets/Scripts/AdministradorJuego.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdministradorJuego : MonoBehaviour
+{
+	public static AdministradorJuego SingletonAdiministradorJuego;
+	
+	private static int _VelocidadBala = 30;
+	public int VelocidadBala
+	{
+		get => _VelocidadBala;
+		set => _VelocidadBala = value;
+	}
+	
+	private static int _DisparosPorJuego = 10;
+	public int DisparosPorJuego
+	{
+		get => _DisparosPorJuego;
+		set => _DisparosPorJuego = value;
+	}
+	
+	private static float _VelocidadRotacion = 1;
+	public float VelocidadRotacion
+	{
+		get => _VelocidadRotacion;
+		set => _VelocidadRotacion = value;
+	}
+	
+	void Awake()
+	{
+		if (SingletonAdiministradorJuego == null)
+		{
+			SingletonAdiministradorJuego = this;
+		}
+		else
+		{
+			Debug.Log("Ya existe una instancia de esta clase");
+		}
+	}
+}
---
+++ GameArtillery/Assets/Scripts/Canon.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canon : MonoBehaviour
+{
+	[SerializeField]
+	GameObject BalaPrefab;
+	GameObject puntaCanon;
+	float rotaticion;
+	
+	int totalDisparos;
+	
+    void Start()
+    {
+	    puntaCanon = transform.Find("PuntaCanon").gameObject;
+	    totalDisparos = AdministradorJuego.SingletonAdiministradorJuego.DisparosPorJuego;
+    }
+
+    void Update()
+    {
+	    rotaticion += Input.GetAxis("Horizontal") * AdministradorJuego.SingletonAdiministradorJuego.VelocidadRotacion;
+	    if (rotaticion <= 90 && rotaticion >= 0)
+	    {
+	    	transform.eulerAngles = new Vector3(rotaticion, 90, 0f);
+	    }
+	    if (rotaticion > 90)
+	    {
+	    	rotaticion = 90;
+	    }
+	    if (rotaticion < 0)
+	    {
+	    	rotaticion = 0;
+	    }
+	    
+	    if (Input.GetKeyDown(KeyCode.Space) &&  totalDisparos > 0)
+	    {
+	    	GameObject temp = Instantiate(BalaPrefab, puntaCanon.transform.position, transform.rotation);
+	    	Rigidbody tempRB = temp.GetComponent<Rigidbody>();
+	    	Vector3 direccionDisparo = transform.rotation.eulerAngles;
+	    	direccionDisparo.y = 90 - direccionDisparo.x;
+	    	tempRB.velocity = direccionDisparo.normalized * AdministradorJuego.SingletonAdiministradorJuego.VelocidadBala;
+	    	totalDisparos--;
+	    }
+    }
+}
