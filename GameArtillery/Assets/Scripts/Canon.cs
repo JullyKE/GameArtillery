9f5c4c9e55f7b412c76997a03813ce27b4e894c6
Jully Kado <jully_000@hotmail.com>
2022-03-23 08:55:36 -0600

    Visual
    particulas,shader y sonido

--- GameArtillery/Assets/Scripts/Canon.cs
+++ GameArtillery/Assets/Scripts/Canon.cs
@@ -8,18 +8,28 @@ public class Canon : MonoBehaviour
 	GameObject BalaPrefab;
 	GameObject puntaCanon;
 	float rotaticion;
+	[SerializeField]
+	GameObject ParticulaDisparo;
+	public AudioClip clipDisparo;
+	AudioSource SourceDisparo;
+	public AudioClip clipExplosion;
+	AudioSource SourceExplocion;
 	
+	AdministradorJuego adminJuego;
 	int totalDisparos;
 	
     void Start()
-    {
+	{
+		adminJuego = GameObject.Find("AdministradorJuego").GetComponent<AdministradorJuego>();
 	    puntaCanon = transform.Find("PuntaCanon").gameObject;
-	    totalDisparos = AdministradorJuego.SingletonAdiministradorJuego.DisparosPorJuego;
+		totalDisparos = adminJuego.DisparosPorJuego; 
+		SourceDisparo = GameObject.Find("SonidoDisparo").GetComponent<AudioSource>();
+		SourceExplocion = GameObject.Find("SonidoExplosion").GetComponent<AudioSource>();
     }
 
     void Update()
     {
-	    rotaticion += Input.GetAxis("Horizontal") * AdministradorJuego.SingletonAdiministradorJuego.VelocidadRotacion;
+	    rotaticion += Input.GetAxis("Horizontal") * adminJuego.VelocidadRotacion;
 	    if (rotaticion <= 90 && rotaticion >= 0)
 	    {
 	    	transform.eulerAngles = new Vector3(rotaticion, 90, 0f);
@@ -33,14 +43,20 @@ public class Canon : MonoBehaviour
 	    	rotaticion = 0;
 	    }
 	    
-	    if (Input.GetKeyDown(KeyCode.Space) &&  totalDisparos > 0)
+	    if (Input.GetKeyDown(KeyCode.Space) &&  totalDisparos > 0 && !adminJuego.Bloqueado)
 	    {
 	    	GameObject temp = Instantiate(BalaPrefab, puntaCanon.transform.position, transform.rotation);
 	    	Rigidbody tempRB = temp.GetComponent<Rigidbody>();
+	    	SeguirCamara.objetivo = temp;
 	    	Vector3 direccionDisparo = transform.rotation.eulerAngles;
 	    	direccionDisparo.y = 90 - direccionDisparo.x;
-	    	tempRB.velocity = direccionDisparo.normalized * AdministradorJuego.SingletonAdiministradorJuego.VelocidadBala;
+	    	Vector3 direccionParticula = new Vector3(-90 + direccionDisparo.x,90,0);
+	    	GameObject particula = Instantiate(ParticulaDisparo,puntaCanon.transform.position,Quaternion.Euler(direccionParticula));
+	    	tempRB.velocity = direccionDisparo.normalized * adminJuego.VelocidadBala;
+	    	SourceDisparo.PlayOneShot(clipDisparo);
+	    	SourceExplocion.PlayOneShot(clipExplosion);
 	    	totalDisparos--;
+	    	adminJuego.Bloqueado = true;
 	    }
     }
 }
